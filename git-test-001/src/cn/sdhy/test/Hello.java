c61c01137971d415a911595917205b9e6feb68df
kelvin <571401620@qq.com>
2019-11-03 15:34:07 +0800

    first to git

---
+++ git-test-001/src/cn/sdhy/test/Hello.java
@@ -0,0 +1,14 @@
+package cn.sdhy.test;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("the first project to git");
+        System.out.println("the first project to git");
+        System.out.println("the first project to git");
+        System.out.println("the first project to git");
+        System.out.println("the first project to git");
+        System.out.println("the first project to git");
+        System.out.println("the first project to git");
+    }
+}
