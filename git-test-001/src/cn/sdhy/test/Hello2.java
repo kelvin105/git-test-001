137d9bb2ee97f39f8d02f1ec343870ec7965a55a
kelvin <571401620@qq.com>
2019-11-03 16:18:36 +0800

    添加hello2

---
+++ git-test-001/src/cn/sdhy/test/Hello2.java
@@ -0,0 +1,13 @@
+package cn.sdhy.test;
+
+public class Hello2 {
+
+    public void test(){
+        int i = 0;
+        System.out.println("zhe bushi ouran :"+i);
+        System.out.println("thank you");
+        System.out.println("thank you");
+        System.out.println("thank you");
+        System.out.println("thank you");
+    }
+}
