042c28112fb2d19149d3f76a1f12808b3d677b51
kelvin <571401620@qq.com>
2019-11-04 18:04:47 +0800

    鬼谷纵横3

--- git-test-001/src/cn/sdhy/test/Hello3.java
+++ git-test-001/src/cn/sdhy/test/Hello3.java
@@ -4,5 +4,7 @@ public class Hello3 {
 
     public static void main(String[] args) {
         System.out.println("鬼谷纵横璧有瑕");
+        System.out.println("鬼谷纵横璧有瑕");
+        System.out.println("鬼谷纵横璧有瑕");
     }
 }
