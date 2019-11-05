5a032b262ad50118b7adcfc72f404f1dc9942b57
kelvin <571401620@qq.com>
2019-11-05 14:02:54 +0800

    002提交1105

---
+++ git-test-001/src/cn/sdhy/test/pojo/SearchItem.java
@@ -0,0 +1,15 @@
+package cn.sdhy.test.pojo;
+
+import java.io.Serializable;
+
+public class SearchItem implements Serializable {
+    private Integer num;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+}
