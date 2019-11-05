a4f1c58e1d1ac69904d56538c38b56d9c99d7f81
kelvin <571401620@qq.com>
2019-11-05 13:59:21 +0800

    003提交11059

--- git-test-001/src/cn/sdhy/test/Hello2.java
+++ git-test-001/src/cn/sdhy/test/Hello2.java
@@ -4,6 +4,9 @@ public class Hello2 {
 
     public static void main(String[] args) {
         System.out.println("陶朱商经石中玉");
+        System.out.println("陶朱商经石中玉");
+        System.out.println("大商之道何处寻");
+        System.out.println("半部论语治天下");
     }
 
 }
